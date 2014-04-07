acf520013990a102ba0c1b59a8723b2bad5be3fe
Albert <alvaltor@gmail.com>
2014-04-07 12:37:29 +0100

    Update and rename sc to script.js

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+/***************************************************************************************
+
+Switching the icons based on the location.href, first case is for the root domain name 
+
+*****************************************************************************************/
+
 var href = location.href.toLowerCase();
   
 switch(href){
+     
      case 'http://residencia-geriatrica-ergos.com/':
        $('li a[href="index.html"]').prepend('<span><i class="icon-home"></i></span>'); //append active icon
        $('li:not(.active) a').prepend('<i class="icon-double-angle-right"></i>'); //append standard icons
      break;
+     
      case 'http://residencia-geriatrica-ergos.com/index.html':
        $('li a[href="index.html"]').prepend('<span><i class="icon-home"></i></span>');
        $('li:not(.active) a').prepend('<i class="icon-double-angle-right"></i>'); //append standard icons
      break;
+     
      case 'http://residencia-geriatrica-ergos.com/servi.html':
        $('li a[href="servi.html"]').prepend('<span><i class="icon-user-md"></i></span>');
       $('li:not(.active) a').prepend('<i class="icon-double-angle-right"></i>'); //append standard icons
      break;
+     
      case 'http://residencia-geriatrica-ergos.com/acti.html':
        $('li a[href="acti.html"]').prepend('<span><i class="icon-group"></i></span>');
        $('li:not(.active) a').prepend('<i class="icon-double-angle-right"></i>'); //append standard icons
      break;
+     
      case 'http://residencia-geriatrica-ergos.com/contact.html':
        $('li a[href="contact.html"]').prepend('<span><i class="icon-envelope"></i></span>');
        $('li:not(.active) a').prepend('<i class="icon-double-angle-right"></i>'); //append standard icons
